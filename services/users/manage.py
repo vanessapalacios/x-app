3ad336e734cb11ae608689f6018751ed8e4e945e
Jhordyho <huamanjhordy@gmail.com>
2019-05-13 19:05:41 -0500

    cobetura

--- services/users/manage.py
+++ services/users/manage.py
@@ -2,11 +2,22 @@
 
 
 import unittest
+import coverage
 
 from flask.cli import FlaskGroup
 
 from project import create_app, db # new
 from project.api.models import User # new
+
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*',
+        'project/config.py',
+    ]
+)
+COV.start()
 
 app = create_app() # new
 cli = FlaskGroup(create_app=create_app) # new
@@ -27,6 +38,22 @@ def test():
     if result.wasSuccessful():
         return 0
     return 1
+
+@cli.command()
+def cov():
+    """Ejecuta las pruebas unitarias con coverage."""
+    tests = unittest.TestLoader().discover('project/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Resumen de cobertura:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    sys.exit(result)
+    
 @cli.command('seed_db')
 def seed_db():
     """Sembrado de la base de datos."""
