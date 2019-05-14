81197759dafc51a7ae242f5157bfe3afdfeff87d
vanessapalacios <fiorellapalacios@upeu.edu.pe>
2019-05-13 22:38:24 -0500

    travis

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,36 +1,27 @@
 # services/users/project/__init__.py
+import os  # nuevo
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy  # nuevo
+# instanciado la app
+app = Flask(__name__)
+
+#  establecer configuraicon
+app_settings = os.getenv('APP_SETTINGS')  # Nuevo
+app.config.from_object(app_settings)      # Nuevo
+
+# instanciando la db
+db = SQLAlchemy(app)  # nuevo
 
 
-import os
-
-from flask import Flask # new
-from flask_sqlalchemy import SQLAlchemy
-
-
-# instantiate the db
-db = SQLAlchemy()
-
-
-# new
 def create_app(script_info=None):
-
-    # instantiate the app
-    app = Flask(__name__) 
-
-    # set config
+    app = Flask(__name__)
     app_settings = os.getenv('APP_SETTINGS')
     app.config.from_object(app_settings)
-
-    # set up extensions
     db.init_app(app)
-
-    # register blueprints
     from project.api.users import users_blueprint
     app.register_blueprint(users_blueprint)
 
-    # shell context for flask cli
     @app.shell_context_processor
     def ctx():
         return {'app': app, 'db': db}
-    
     return app
