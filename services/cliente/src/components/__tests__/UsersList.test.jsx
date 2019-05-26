9db212fec0af9eec6e4e6e6ee6129b08f55b1917
vanessapalacios <fiorellapalacios@upeu.edu.pe>
2019-05-26 17:31:23 -0500

    Prod ReacJS

--- services/cliente/src/components/__tests__/UsersList.test.jsx
+++ services/cliente/src/components/__tests__/UsersList.test.jsx
@@ -18,6 +18,14 @@ const users = [
         'username': 'vanessa'
     }
 ];
+
+test('UsersList renders properly', () => {
+    const wrapper = shallow(<UsersList users={users}/>);
+    const element = wrapper.find('h4')
+    expect(element.length).toBe(2);
+    expect(element.get(0).props.children).toBe('fiorella');
+});
+
 test('UsersList renders properly', () => {
     const tree = renderer.create(<UsersList users={users}/>).toJSON();
     expect(tree).toMatchSnapshot();
--- services/cliente/src/index.js
+++ services/cliente/src/index.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import UsersList from './components/UsersList';
-
+import AddUser from './components/AddUser';
 
 class App extends Component {
     constructor() {
       super();
       this.state = {
-          users:[]
+          users:[],
+          username:'justatest',
+          email:'',
       };
+      this.addUser = this.addUser.bind(this);
+      this.handleChange = this.handleChange.bind(this);
     };
     componentDidMount() {
         this.getUsers();
@@ -21,18 +25,45 @@ class App extends Component {
       .then((res) => { this.setState({ users: res.data.data.users }); })
       .catch((err) => { console.log(err); });
     };
+
+    addUser(event){
+      event.preventDefault();
+      const data = {
+        username: this.state.username,
+        email: this.state.email
+      };
+      axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`,data)
+      .then((res) => { 
+        this.getUsers();
+        this.setState({username:'',email:''});
+      })
+      .catch((err) => { console.log(err)});
+    }
+
+    handleChange(event){
+      const obj = {};
+      obj[event.target.name] = event.target.value;
+      this.setState(obj);
+    }
     render() {
         return (
           <section className="section">
 	        <div className="container">
-              <div className="columns">
+            <div className="columns">
 		        <div className="column is-one-third">
 		          <br/>
 		          <h1 className="title is-1">Todos los usuarios</h1>
 		          <hr/><br/>
+              <AddUser 
+              username={this.state.username}
+              email={this.state.email}
+              addUser={this.addUser}
+              handleChange={this.handleChange}
+              />
+              <hr/><br/>
                   {/* new */}
                   <UsersList users={this.state.users}/>
-	        </div>
+	          </div>
           </div>
         </div>
       </section>
