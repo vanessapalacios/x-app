708fdb4f9a3a8b0076186ee1a9ae85e77f2ecd2f
vanessapalacios <fiorellapalacios@upeu.edu.pe>
2019-05-28 15:30:07 -0500

    avance

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -4,6 +4,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy  # nuevo
 from flask_debugtoolbar import DebugToolbarExtension
 from flask_cors import CORS
+from flask_migrate import Migrate
+
 # instanciado la app
 app = Flask(__name__)
 
@@ -15,6 +17,7 @@ app.config.from_object(app_settings)  # Nuevo
 db = SQLAlchemy(app)
 toolbar = DebugToolbarExtension()
 cors = CORS()
+migrate = Migrate()
 
 
 def create_app(script_info=None):
@@ -24,10 +27,13 @@ def create_app(script_info=None):
     db.init_app(app)
     toolbar.init_app(app)
     cors.init_app(app)
+    migrate.init_app(app, db)
+
     from project.api.users import users_blueprint
     app.register_blueprint(users_blueprint)
 
     @app.shell_context_processor
     def ctx():
         return {'app': app, 'db': db}
-    return app
+
+        return app
